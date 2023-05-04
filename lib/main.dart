d20d8a4978029d01a3b7b9ec9fd1a08d54453933
Habib Asseiss <habibasseiss@gmail.com>
2023-05-04 18:04:34 -0400

    04/05

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:meuapp/pagina_inicial.dart';
+import 'pagina_inicial.dart';
 
 void main() {
   runApp(const MyApp());
--- lib/pagina_inicial.dart
+++ lib/pagina_inicial.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: prefer_const_constructors, avoid_print, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:meuapp/conversas.dart';
+import 'conversas.dart';
+import 'status.dart';
+import 'chamadas.dart';
 
 class PaginaInicial extends StatelessWidget {
   const PaginaInicial({super.key});
@@ -9,45 +11,52 @@ class PaginaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Color.fromARGB(255, 56, 127, 107),
-            title: Text("WhatsApp do IF"),
-            actions: [
-              IconButton(
-                onPressed: () {
-                  print("O botão da câmera foi clicado.");
-                },
-                icon: Icon(Icons.camera_alt_outlined),
-              ),
-              IconButton(
-                onPressed: () {
-                  print("O botão da busca foi clicado.");
-                },
-                icon: Icon(Icons.search),
-              ),
-              IconButton(
-                onPressed: () {
-                  print("O botão mais foi clicado.");
-                },
-                icon: Icon(Icons.more_vert),
-              ),
-            ],
-            bottom: TabBar(
-              tabs: [
-                Tab(text: 'Conversas'),
-                Tab(text: 'Ligações'),
-                Tab(text: 'Status'),
-              ],
+      length: 3, // número de abas do app
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 56, 127, 107),
+          title: Text("WhatsApp do IF"),
+          actions: [
+            IconButton(
+              onPressed: () {
+                print("O botão da câmera foi clicado.");
+              },
+              icon: Icon(Icons.camera_alt_outlined),
             ),
+            IconButton(
+              onPressed: () {
+                print("O botão da busca foi clicado.");
+              },
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: () {
+                print("O botão mais foi clicado.");
+              },
+              icon: Icon(Icons.more_vert),
+            ),
+          ],
+          bottom: TabBar(
+            tabs: [
+              Tab(text: 'Conversas'),
+              Tab(text: 'Status'),
+              Tab(text: 'Chamadas'),
+            ],
           ),
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Color.fromARGB(255, 56, 127, 107),
-            onPressed: () {},
-            child: Icon(Icons.chat, size: 32),
-          ),
-          body: Conversas(),
-        ));
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Color.fromARGB(255, 56, 127, 107),
+          onPressed: () {},
+          child: Icon(Icons.chat, size: 32),
+        ),
+        body: TabBarView(
+          children: [
+            Conversas(),
+            Status(),
+            Chamadas(),
+          ],
+        ),
+      ),
+    );
   }
 }
